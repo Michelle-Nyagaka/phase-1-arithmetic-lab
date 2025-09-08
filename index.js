180b17d62c2c2d600cacc45e89b3a4c11566898f
Michelle Nyagaka <michellenyagaka@gmail.com>
2025-09-08 16:02:01 +0300

    Arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+//1.
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+//2.
+const random = Math.floor(Math.random()*10)+1
+//3.
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+//4.
+const max = Math.max(1,5,10,20);
+
